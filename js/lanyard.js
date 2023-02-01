7a4521b45dec7257541e8feda7e8973e35046ee1
hozzywozzy <87353619+hozzywozzy@users.noreply.github.com>
2023-02-01 08:35:14 -0500

    Update lanyard.js

--- js/lanyard.js
+++ js/lanyard.js
@@ -142,7 +142,7 @@ async function setActivityName() {
     } = await fetchResponse(USERID);
     const mostRecent = activities.filter(m => m.type !== 4).shift();
     if (!mostRecent?.name) {
-        name.innerHTML = 'doing yo moma';
+        name.innerHTML = 'failing school';
         return;
     }
     name.style.display = 'block';
